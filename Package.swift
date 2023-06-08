a7adc289660ed4313170e32399466533c545fb57
Varun Santhanam <talkto@vsanthanam.com>
2023-06-08 11:28:41 -0400

    Add Macros

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import CompilerPluginSupport
 import PackageDescription
 
 let package = Package(
@@ -16,21 +17,47 @@ let package = Package(
             name: "Outils",
             targets: ["Outils"]
         ),
+        .executable(
+            name: "Client",
+            targets: ["Client"]
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", exact: "0.51.7")
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", exact: "0.51.7"),
+        // Depend on the latest Swift 5.9 prerelease of SwiftSyntax
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0-swift-5.9-DEVELOPMENT-SNAPSHOT-2023-04-25-b"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Outils",
-            dependencies: []
+            dependencies: ["OutilsMacros"]
+        ),
+        .macro(
+            name: "OutilsMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
+        .executableTarget(
+            name: "Client",
+            dependencies: ["Outils"]
         ),
         .testTarget(
             name: "OutilsTests",
-            dependencies: ["Outils"]
+            dependencies: [
+                "Outils"
+            ]
         ),
+        .testTarget(
+            name: "OutilsMacrosTests",
+            dependencies: [
+                "OutilsMacros",
+                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+            ]
+        )
     ]
 )
